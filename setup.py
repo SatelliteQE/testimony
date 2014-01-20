033ceda5aaa4e64c53f9c44e2e72b551289131f0
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-01-20 20:36:45 -0200

    Update setup.py
    Update setup.py to read long_description from README.rst

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
+with open('README.rst') as file:
+    long_description = file.read()
+
 setup(
     name='testimony',
     version='0.1.0',
     url='https://github.com/sthirugn/testimony/',
     author='Suresh Thirugn',
     author_email='sthirugn@redhat.com',
-    packages=['testimony', ],
+    packages=['testimony'],
     scripts=['bin/testimony'],
-    long_description=open('README.md').read(),
+    long_description=long_description,
 )
