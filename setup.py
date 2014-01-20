f17e0e34583913881be01d0098b45f70472b381d
sthirugn <sthirugn@redhat.com>
2014-01-20 16:24:00 -0500

    Added metadata

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 
 setup(
     name='testimony',
-    version='0.1dev',
-    packages=['testimony',],
+    version='0.1.0',
+    url='https://github.com/sthirugn/testimony/',
+    author='Suresh Thirugn',
+    author_email='sthirugn@redhat.com',
+    packages=['testimony', ],
     scripts=['bin/testimony'],
     long_description=open('README.md').read(),
 )
