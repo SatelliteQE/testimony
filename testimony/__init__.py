ceb075f0a94586889e84a101a003f583921aba53
sthirugn <sthirugn@redhat.com>
2014-01-23 14:15:47 -0500

    Fixes the following: Now parses the folders under the given path recursively, Ignores the case for the docstring attributes, Global variables removed

--- testimony/__init__.py
+++ testimony/__init__.py
@@ -11,15 +11,6 @@ import os
 from testimony.constants import DOCSTRING_TAGS, REPORT_TAGS
 
 
-bugs = 0
-bug_list = []
-invalid_doc_string = 0
-manual_count = 0
-no_doc_string = 0
-tc_count = 0
-userinput = None
-
-
 def main(report, paths):
     """
     Main function for testimony project
@@ -27,56 +18,49 @@ def main(report, paths):
     Expects a valid report type and valid directory paths, hopefully argparse
     is taking care of validation
     """
-    global bug_list
-    global bugs
-    global invalid_doc_string
-    global userinput
-
-    userinput = report
+    result = {
+        'bugs': 0,
+        'bugs_list': list(),
+        'invalid_docstring': 0,
+        'manual_count': 0,
+        'tc_count': 0,
+        }
+    dir_list = []
     for path in paths:
-        reset_counts()
-        print "\nTEST PATH: %s" % path
+        result = reset_counts(result)
         print "--------------------------------------------------------------"
-        for root, dirs, files in os.walk(path):
-            for i in range(1, len(files)):  # Loop for each file
-                if str(files[i]).startswith('test_') and str(files[i]).endswith('.py'):  # @IgnorePep8
+        dir_list.append(path)
+        dir_list = get_all_dirs(path, dir_list)
+        for dirs in dir_list:
+            print "\nTEST PATH: %s" % dirs
+            for files in os.listdir(str(dirs)):
+                if str(files).startswith('test_') and str(files).endswith('.py'):
                     #Do not print this text for test summary
-                    if userinput != REPORT_TAGS[1]:
-                        print "Analyzing %s..." % files[i]
-                    filepath = os.path.join(os.path.abspath(path), files[i])
-                    list_strings = get_docstrings(filepath)
-                    if userinput in REPORT_TAGS[0] or userinput in REPORT_TAGS[2:3]:
-                        #print the derived test cases
-                        print_testcases(list_strings)
-                    elif userinput == REPORT_TAGS[4]:
-                        #print manual test cases
-                        print_testcases(list_strings, test_type="manual")
-                    elif userinput == REPORT_TAGS[5]:
-                        #print auto test cases
-                        print_testcases(list_strings, test_type="auto")
+                    if report != REPORT_TAGS[1]:
+                        print "Analyzing %s..." % files
+                    filepath = os.path.join(str(dirs), str(files))
+                    list_strings, result = get_docstrings(report, filepath, result)
+                    if report != REPORT_TAGS[1]:
+                        print_testcases(report, list_strings, result)
                     else:
                         #for printing test summary later
-                        update_summary(list_strings)
+                        result = update_summary(list_strings, result)
         #Print for test summary
-        if userinput == REPORT_TAGS[1]:
-            print_summary()
+        if report == REPORT_TAGS[1]:
+            print_summary(result)
         #Print total number of invalid doc strings
-        if userinput == REPORT_TAGS[2]:
-            print "Total Number of invalid docstrings:  %d" % invalid_doc_string  # @IgnorePep8
+        if report == REPORT_TAGS[2]:
+            print "\nTotal Number of invalid docstrings:  %d" % result['invalid_docstring']
         #Print number of test cases affected by bugs and also the list of bugs
-        if userinput == REPORT_TAGS[3]:
-            print "Total Number of test cases affected by bugs: %d" % bugs
-            print "List of bugs:                               ", bug_list
+        if report == REPORT_TAGS[3]:
+            print "\nTotal Number of test cases affected by bugs: %d" % result['bugs']
+            print "List of bugs:                               ", result['bug_list']
 
 
-def get_docstrings(path):
+def get_docstrings(report, path, result):
     """
     Function to read docstrings from test_*** methods for a given file
     """
-    global no_doc_string
-    global invalid_doc_string
-    global bugs
-    global bug_list
     return_list = []
     obj = ast.parse(''.join(open(path)))
     #The body field inside obj.body[] contains the docstring
@@ -109,107 +93,96 @@ def get_docstrings(path):
                             #Remove any new line characters
                             attr = attr.rstrip('\n')
                             if attr != '':
-                                if userinput == REPORT_TAGS[2]:
+                                if report == REPORT_TAGS[2]:
                                     docstring_tag = attr.split(" ", 1)
                                     #Error out invalid docstring
-                                    if not any(x in docstring_tag[0] for x in DOCSTRING_TAGS):  # @IgnorePep8
-                                        item_list.append("-->Invalid DocString: %s<--" % attr)  # @IgnorePep8
-                                        invalid_doc_string = invalid_doc_string + 1  # @IgnorePep8
-                                elif userinput == REPORT_TAGS[3]:
+                                    if not any(x in docstring_tag[0].lower() for x in DOCSTRING_TAGS):
+                                        item_list.append("-->Invalid DocString: %s<--" % attr)
+                                        result['invalid_docstring'] = result['invalid_docstring'] + 1
+                                elif report == REPORT_TAGS[3]:
                                     #Find the bug from docstring
                                     docstring_tag = attr.split(" ", 1)
-                                    if DOCSTRING_TAGS[5] in docstring_tag[0]:
+                                    if DOCSTRING_TAGS[5] in docstring_tag[0].lower():
                                         item_list.append(attr)
-                                        bugs = bugs + 1
-                                        bug_list.append(docstring_tag[1])
+                                        result['bugs'] = result['bugs'] + 1
+                                        result['bug_list'].append(docstring_tag[1])
                                 else:
                                     #For printing all test cases
                                     item_list.append(attr)
                         if len(item_list) != 0:
                             return_list.append(item_list)
         except AttributeError:
-            if userinput == REPORT_TAGS[0] or userinput == REPORT_TAGS[2]:
+            if report == REPORT_TAGS[0] or report == REPORT_TAGS[2]:
                 print "--> DocString Missing. Please update <--"
-            no_doc_string = no_doc_string + 1
+            result['no_docstring'] = result['no_docstring'] + 1
             continue
         except:
             print "!!!!!Exception in parsing DocString!!!!!"
-    return return_list
+    return return_list, result
 
 
-def print_testcases(list_strings, test_type=None):
+def print_testcases(report, list_strings, result):
     """
     Prints all the test cases based on given criteria
     """
-    global userinput
     tc = 0
     for docstring in list_strings:
-        if userinput == REPORT_TAGS[0]:
+        if report == REPORT_TAGS[0]:
             tc = tc + 1
             print "TC %d" % tc
 
         #verify if this needs to be printed
-        if test_type is not None:
-            manual_print = False
-            auto_print = True
-            for lineitem in docstring:
-                docstring_tag = lineitem.split(" ", 1)
-                if test_type == "auto":
-                    if DOCSTRING_TAGS[6] in docstring_tag[0]:
-                        auto_print = False
-                if test_type == "manual":
-                    if DOCSTRING_TAGS[6] in docstring_tag[0]:
-                        manual_print = True
-        if test_type == "auto" and auto_print is True:
+        manual_print = False
+        auto_print = True
+        for lineitem in docstring:
+            docstring_tag = lineitem.split(" ", 1)
+            if report == REPORT_TAGS[5]:
+                if DOCSTRING_TAGS[6] in docstring_tag[0].lower():
+                    auto_print = False
+            if report == REPORT_TAGS[4]:
+                if DOCSTRING_TAGS[6] in docstring_tag[0].lower():
+                    manual_print = True
+        if report == REPORT_TAGS[5] and auto_print is True:
             print_line_item(docstring)
-        if test_type == "manual" and manual_print is True:
+        if report == REPORT_TAGS[4] and manual_print is True:
             print_line_item(docstring)
-        if userinput == REPORT_TAGS[0] or userinput == REPORT_TAGS[2]:
+        if report == REPORT_TAGS[0] or report == REPORT_TAGS[2]:
             print_line_item(docstring)
 
 
-def update_summary(list_strings):
+def update_summary(list_strings, result):
     """
     Updates summary for reporting
     """
-    global tc_count
-    global manual_count
     for docstring in list_strings:
-        tc_count = tc_count + 1
+        result['tc_count'] = result['tc_count'] + 1
         for lineitem in docstring:
             if lineitem.startswith("Status") and "Manual" in lineitem:
-                manual_count = manual_count + 1
+                result['manual_count'] = result['manual_count'] + 1
+    return result
 
 
-def print_summary():
+def print_summary(result):
     """
     Prints summary for reporting
     """
-    global tc_count
-    global manual_count
-    global no_doc_string
-    print "Total Number of test cases:      %d" % tc_count
-    print "Total Number of automated cases: %d" % (tc_count - manual_count)
-    print "Total Number of manual cases:    %d" % manual_count
-    print "Test cases with no docstrings:   %d" % no_doc_string
+    print "Total Number of test cases:      %d" % result['tc_count']
+    print "Total Number of automated cases: %d" % (result['tc_count'] - result['manual_count'])
+    print "Total Number of manual cases:    %d" % result['manual_count']
+    print "Test cases with no docstrings:   %d" % result['no_docstring']
 
 
-def reset_counts():
+def reset_counts(result):
     """
     Resets all the counts to switch between UI and CLI reports
     """
-    global tc_count
-    global manual_count
-    global no_doc_string
-    global invalid_doc_string
-    global bugs
-    global bug_list
-    tc_count = 0
-    manual_count = 0
-    no_doc_string = 0
-    invalid_doc_string = 0
-    bugs = 0
-    bug_list = []
+    result['tc_count'] = 0
+    result['manual_count'] = 0
+    result['no_docstring'] = 0
+    result['invalid_docstring'] = 0
+    result['bugs'] = 0
+    result['bug_list'] = []
+    return result
 
 
 def print_line_item(docstring):
@@ -220,9 +193,13 @@ def print_line_item(docstring):
         print lineitem
 
 
-def get_root_path():
+def get_all_dirs(path, dir_list):
     """
-    Returns correct path to logging config file
+    Returns all folders recursively
     """
-    return os.path.realpath(
-        os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+    for root, dirs, files in os.walk(path):
+        for attr in dirs:
+            attr = os.path.join(os.path.abspath(root), attr)
+            if os.path.isdir(attr):
+                dir_list.append(attr)
+    return dir_list
