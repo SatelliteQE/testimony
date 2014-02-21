ff4b4ec3b440f09eb7dda1d74be02a655873a6ea
Elyézer Mendes Rezende <elyezermr@gmail.com>
2014-02-21 09:22:31 -0300

    Add license and description to setup definition

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 
+with open('LICENSE') as file:
+    license = file.read()
+
 with open('README.rst') as file:
     long_description = file.read()
 
@@ -11,5 +14,7 @@ setup(
     author_email='sthirugn@redhat.com',
     packages=['testimony'],
     scripts=['bin/testimony'],
+    description='Testimony inspects and reports on the python test cases.',
     long_description=long_description,
+    license=license,
 )
