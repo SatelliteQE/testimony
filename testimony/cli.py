fde48b1021f31d185075c6b1e7f95cc5ed85053a
Jan Hutar <jhutar@redhat.com>
2019-05-16 12:30:16 +0200

    yaml.load() without Loader is deprecated, see https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- testimony/cli.py
+++ testimony/cli.py
@@ -28,5 +28,6 @@ def testimony(
             token.strip().lower() for token in minimum_tokens.split(',')]
     if token_values:
         with open(token_values, 'r') as fp:
-            SETTINGS['token_values'] = {k.lower():v for k,v in yaml.load(fp).items()}
+            data = yaml.load(fp, Loader=yaml.SafeLoader)
+            SETTINGS['token_values'] = {k.lower():v for k,v in data.items()}
     main(report, path, json, nocolor)
