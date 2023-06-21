27c3c159b5f2831bf36db5b75b02e49550a69d8e
Elyézer Rezende <elyezermr@gmail.com>
2023-06-21 12:01:09 -0400

    Support more recent Python versions
    Focus testing on Python 3.8+

--- setup.py
+++ setup.py
@@ -21,7 +21,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     entry_points="""
         [console_scripts]
